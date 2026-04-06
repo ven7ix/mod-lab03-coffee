9007d8fd829262cb2915d32c4606f75ab57bd93a
VANIS <ven7ix@gmail.com>
2026-04-06 16:48:42 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,5 +1,3 @@
-// Copyright 2022 GHA Test Team
-
 #include <gtest/gtest.h>
 #include "Automata.h"
 
@@ -10,4 +8,101 @@ TEST(test_suite_name, test_name) {
 	automata.on();
 
 	EXPECT_EQ("WAIT", automata.get_state());
+}
+
+TEST(test_suite_name, test_name) {
+	Automata automata;
+	std::cout << automata.get_state() << std::endl;
+
+	automata.on();
+	automata.choice(1);
+
+	EXPECT_EQ("WAIT", automata.get_state());
+}
+
+TEST(test_suite_name, test_name) {
+	Automata automata;
+	std::cout << automata.get_state() << std::endl;
+
+	automata.on();
+	automata.coin(0);
+	automata.choice(1);
+
+	EXPECT_EQ("CHECK", automata.get_state());
+}
+
+TEST(test_suite_name, test_name) {
+	Automata automata;
+	std::cout << automata.get_state() << std::endl;
+
+	automata.on();
+	automata.coin(0);
+	automata.choice(10);
+
+	EXPECT_EQ("ACCEPT", automata.get_state());
+}
+
+TEST(test_suite_name, test_name) {
+	Automata automata;
+	std::cout << automata.get_state() << std::endl;
+
+	automata.on();
+	automata.coin(0);
+	automata.choice(1);
+	automata.cancel();
+
+	EXPECT_EQ("WAIT", automata.get_state());
+}
+
+TEST(test_suite_name, test_name) {
+	Automata automata;
+	std::cout << automata.get_state() << std::endl;
+
+	automata.on();
+	automata.coin(10);
+
+	EXPECT_EQ(10, automata.get_cash());
+}
+
+TEST(test_suite_name, test_name) {
+	Automata automata;
+	std::cout << automata.get_state() << std::endl;
+
+	automata.on();
+	automata.coin(10);
+	automata.coin(-5);
+
+	EXPECT_EQ(10, automata.get_cash());
+}
+
+TEST(test_suite_name, test_name) {
+	Automata automata;
+	std::cout << automata.get_state() << std::endl;
+
+	automata.on(10);
+	automata.coin(0);
+
+	EXPECT_EQ(10, automata.get_cash());
+}
+
+TEST(test_suite_name, test_name) {
+	Automata automata;
+	std::cout << automata.get_state() << std::endl;
+
+	automata.on();
+	automata.coin(0);
+	automata.choice(1);
+
+	EXPECT_EQ(false, automata.check());
+}
+
+TEST(test_suite_name, test_name) {
+	Automata automata;
+	std::cout << automata.get_state() << std::endl;
+
+	automata.on();
+	automata.coin(100);
+	automata.choice(1);
+
+	EXPECT_EQ(true, automata.check());
 }
