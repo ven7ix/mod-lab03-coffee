3f9e860cadb22f91c548bef61bb0eefc2840b27e
VANIS <ven7ix@gmail.com>
2026-04-06 17:08:00 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,35 @@
-﻿#include "Automata.h"
+// Copyright 2026 Andrew
+
+#include "Automata.h"
 #include <iostream>
 
 int main() {
-	Automata automata;
+    Automata automata;
 
-	std::cout << automata.get_state() << std::endl;
+    std::cout << automata.get_state() << std::endl;
 
-	automata.on();
-	std::cout << automata.get_state() << std::endl;
+    automata.on();
+    std::cout << automata.get_state() << std::endl;
 
-	automata.coin(50);
-	std::cout << automata.get_state() << std::endl;
-	automata.cancel();
-	std::cout << automata.get_state() << std::endl;
-	automata.coin(10);
-	std::cout << automata.get_state() << std::endl;
-	automata.coin(1);
-	std::cout << automata.get_state() << std::endl;
+    automata.coin(50);
+    std::cout << automata.get_state() << std::endl;
+    automata.cancel();
+    std::cout << automata.get_state() << std::endl;
+    automata.coin(10);
+    std::cout << automata.get_state() << std::endl;
+    automata.coin(1);
+    std::cout << automata.get_state() << std::endl;
 
-	automata.choice(1);
-	std::cout << automata.get_state() << std::endl;
-	automata.cancel();
-	std::cout << automata.get_state() << std::endl;
+    automata.choice(1);
+    std::cout << automata.get_state() << std::endl;
+    automata.cancel();
+    std::cout << automata.get_state() << std::endl;
 
-	automata.coin(0);
-	std::cout << automata.get_state() << std::endl;
-	automata.choice(2);
-	std::cout << automata.get_state() << std::endl;
-	automata.check();
-	std::cout << automata.get_state() << std::endl;
-	return 0;
+    automata.coin(0);
+    std::cout << automata.get_state() << std::endl;
+    automata.choice(2);
+    std::cout << automata.get_state() << std::endl;
+    automata.check();
+    std::cout << automata.get_state() << std::endl;
+    return 0;
 }
