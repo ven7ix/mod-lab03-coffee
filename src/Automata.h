5ec9dc0fc263cb23430495becc137d5f76f76635
VANIS <ven7ix@gmail.com>
2026-04-06 16:22:35 +0300

    Add files via upload

---
+++ src/Automata.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+enum AUTOMATA_STATES {
+	OFF,
+	WAIT,
+	ACCEPT,
+	CHECK,
+	COOK
+};
+
+struct Item {
+	std::string name;
+	double price;
+};
+
+class Automata {
+public:
+	void on();
+	void off();
+	void coin(double shekelz);
+	void cancel();
+	void choice(unsigned int menu_item);
+	bool check();
+	void cook();
+	void finish();
+	std::string get_state();
+private:
+	AUTOMATA_STATES state = OFF;
+	double cash = 0;
+	std::vector<Item> menu = { { "coffee", 10 }, { "tea", 20 }, { "water", 5 } };
+	unsigned int current_menu_item_index = 0;
+};
