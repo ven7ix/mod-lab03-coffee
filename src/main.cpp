63b86e6ec23dcb58d5b266dedac2094ad7b6b90e
VANIS <ven7ix@gmail.com>
2026-04-06 16:23:36 +0300

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+﻿#include "Automata.h"
+#include <iostream>
+
+void main() {
+	Automata automata;
+
+	std::cout << automata.get_state() << std::endl;
+
+	automata.on();
+	std::cout << automata.get_state() << std::endl;
+
+	automata.coin(50);
+	std::cout << automata.get_state() << std::endl;
+	automata.cancel();
+	std::cout << automata.get_state() << std::endl;
+	automata.coin(10);
+	std::cout << automata.get_state() << std::endl;
+	automata.coin(1);
+	std::cout << automata.get_state() << std::endl;
+
+	automata.choice(1);
+	std::cout << automata.get_state() << std::endl;
+	automata.cancel();
+	std::cout << automata.get_state() << std::endl;
+
+	automata.coin(0);
+	std::cout << automata.get_state() << std::endl;
+	automata.choice(2);
+	std::cout << automata.get_state() << std::endl;
+	automata.check();
+	std::cout << automata.get_state() << std::endl;
+}
